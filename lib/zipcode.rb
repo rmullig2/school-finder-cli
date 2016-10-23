0633f2bd1d22832a2f983751a513418d5fc36c82
Ray Mulligan <rmullig2@optonline.net>
2016-10-23 18:07:47 -0400

    Load valid zip codes into array

--- lib/zipcode.rb
+++ lib/zipcode.rb
@@ -1,8 +1,11 @@
 class ZipCode
   attr_accessor :zip
+  attr_reader :valid
 
   def initialize(zip)
     @zip = zip
+    #File.readlines("../zips.txt").each { |zip| @valid << zip.chomp! }
+    @valid = File.open("zips.txt").readlines.each { |zip| zip.chomp! }
   end
-  
+
 end
--- spec/zipcode_spec.rb
+++ spec/zipcode_spec.rb
@@ -4,7 +4,12 @@ require_relative "../lib/zipcode.rb"
 describe 'ZipCode' do
 
   it 'creates a new zip code object' do
-    zipcode = ZipCode.new(11710)
-    expect(zipcode.zip).to eq(11710)
+    zipcode = ZipCode.new("11710")
+    expect(zipcode.zip).to eq("11710")
+  end
+
+  it 'check for database of valid zip codes' do
+    zipcode = ZipCode.new("11710")
+    zipcode.valid.size > 40000
   end
 end
