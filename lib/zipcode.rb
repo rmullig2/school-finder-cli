2b84f55f6860f40cdde66efcf48deca9f7f66acc
Ray Mulligan <rmullig2@optonline.net>
2016-10-24 01:10:43 -0400

    Added range array

--- lib/zipcode.rb
+++ lib/zipcode.rb
@@ -3,9 +3,22 @@ class ZipCode
   attr_reader :valid
 
   def initialize(zip)
-    @zip = zip
-    #File.readlines("../zips.txt").each { |zip| @valid << zip.chomp! }
     @valid = File.open("zips.txt").readlines.each { |zip| zip.chomp! }
+    setZip(zip)
+    @range = []
+    @range << self.zip
+  end
+
+  def setZip(zip)
+    while !@valid.include?(zip)
+      puts "That zip code is invalid, please enter a new one"
+      zip = gets.chomp!
+    end
+    @zip = zip
+  end
+
+  def range
+    @range
   end
 
 end
--- spec/zipcode_spec.rb
+++ spec/zipcode_spec.rb
@@ -12,4 +12,5 @@ describe 'ZipCode' do
     zipcode = ZipCode.new("11710")
     zipcode.valid.size > 40000
   end
+
 end
