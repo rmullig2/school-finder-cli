684a476d74392a4fe599ed8387c42e01dea34368
Ray Mulligan <rmullig2@optonline.net>
2016-10-24 19:26:13 -0400

    Define first test and school object

---
+++ spec/school_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+require_relative "../lib/school.rb"
+
+describe 'School' do
+
+  it 'creates a new school object' do
+    school = School.new(school)
+    expect(zipcode.zip).to eq(11710)
+  end
+end
