638a08b08597b36224f574e05a6a822dab24fe24
Ray Mulligan <rmullig2@optonline.net>
2016-10-24 21:44:45 -0400

    Added zip parameter for school initialization

--- lib/school.rb
+++ lib/school.rb
@@ -10,12 +10,13 @@ class School
   @@list = []
   @@summary = []
 
-  def initialize(school_HTML)
+  def initialize(school_HTML, zip)
     if school_HTML != nil
       page = Nokogiri::HTML(open(school_HTML))
       schools = page.css(".search-result-content")
       schools.each { |school| 
         @name = school.children[1].children[1].children[1].children.text
+        @zip = zip
         @district = school.children[1].children[3].children[3].children.text
         @ranking = school.children[1].children[5].children[3].children.text
         @rank = /\#[0-9]+/.match(@ranking.to_s)
@@ -23,7 +24,7 @@ class School
         @grad_rate = schools.css(".show-for-small-only")[0].children[1].children[1].children.text
         @readiness = schools.css(".show-for-small-only")[0].children[3].children[1].text
         @@summary << { :name => @name, :rank => @rank }
-        @@list << { :name => @name, :district => @district, :rank => @rank, :description => @description,
+        @@list << { :name => @name, :zip => @zip, :district => @district, :rank => @rank, :description => @description,
           :grad_rate => @grad_rate, :readiness => @readiness }
       }
     end
--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -4,13 +4,13 @@ require_relative "../lib/school.rb"
 describe 'School' do
 
   it 'creates a new school object' do
-    school = School.new(nil)
+    school = School.new(nil, "11710")
     expect(school.class).to eq(School)
   end
   
   it 'creates a school object from XML' do
     xml_school = File.read("spec/test.xml")
-    school = School.new("http://www.usnews.com/education/best-high-schools/search?city-or-zip=11710")
+    school = School.new("http://www.usnews.com/education/best-high-schools/search?city-or-zip=11710", "11710")
     expect(school.class).to eq(School)
   end
   
