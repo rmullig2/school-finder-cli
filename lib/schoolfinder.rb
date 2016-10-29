a2e70ccc1f5c0bfbd612e04a5d3ebb3316bb7499
Ray Mulligan <rmullig2@optonline.net>
2016-10-29 17:17:14 -0400

    Start user input testing

--- lib/schoolfinder.rb
+++ lib/schoolfinder.rb
@@ -3,22 +3,36 @@ require 'pry'
 
 class SchoolFinder
 
+  attr_accessor :zip, :miles, :income
+
   def initialize
   end
 
   def call
     puts "Welcome to the School Finder application!"
     puts "Please enter a zip code from where you wish to search"
-    zip = gets.chomp!
-    zipcode = ZipCode.new(zip)
+    @zip = gets.chomp!
+    zipcode = ZipCode.new(@zip)
     #    binding.pry
     puts "Please enter the maximum search radius in miles (0-30)"
-    miles = gets.chomp.to_i
+    @miles = gets.chomp.to_i
+    check("miles", @miles)
     puts "Please enter your income (hit enter to skip this step)"
-    income = gets.chomp.to_i
+    @income = gets.chomp.to_i
     puts "Please enter the number of results you would like to see"
     results = gets.chomp.to_i
     puts "Zip = #{zipcode.zip}, miles = #{miles}, income = #{income}, results = #{results}"
+  end
+
+  def check(check_type, value)
+    case check_type
+    when "miles"
+      while value < 0 || value > 30
+        puts "Please input a number from 0 to 30"
+        value = gets.chomp.to_i
+      end
+      @miles = value
+    end
   end
 
 # CLI for school finder app
